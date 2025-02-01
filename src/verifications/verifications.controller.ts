6a69518ba4aae6bfd6a4559a73ffb3dba1a4e443
Sebastian Guaqueta <sebastian.guaqueta@rootstocklabs.com>
2025-02-01 16:27:00 -0500

    feat/adding complete verification cycle working with solidity source and standard JSON input files

--- src/verifications/verifications.controller.ts
+++ src/verifications/verifications.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { VerificationsService } from './verifications.service';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Multer } from 'multer';
 @Controller('verifications')
 export class VerificationsController {
   constructor(private verificationsService: VerificationsService) {}
@@ -8,5 +16,15 @@ export class VerificationsController {
   @Get('getEvmVersions')
   getEvmVersions() {
     return this.verificationsService.getEvmVersions();
+  }
+
+  @Post('verify')
+  @UseInterceptors(FileInterceptor('file'))
+  async verify(@UploadedFile() file: Multer.File, @Body('data') data: string) {
+    try {
+      return this.verificationsService.verify(data, file);
+    } catch (error) {
+      throw new Error(`Invalid FormData format ${error}`);
+    }
   }
 }
--- src/verifications/verifications.service.ts
+++ src/verifications/verifications.service.ts
@@ -1,14 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { EVM_VERSIONS } from 'src/constants/evm.constants';
 import { PrismaService } from 'src/prisma.service';
+import { VerifyRequestDto } from './verify-request.dto';
+import { Multer } from 'multer';
 
 @Injectable()
 export class VerificationsService {
-  constructor(
-      private prisma: PrismaService,
-    ) {}
-  
-  getEvmVersions() : string[] {
+  constructor(private prisma: PrismaService) {}
+
+  getEvmVersions(): string[] {
     return EVM_VERSIONS;
+  }
+  async verify(data: string, file: Multer.File | undefined) {
+    try {
+      const dataParsed: VerifyRequestDto = JSON.parse(data);
+
+      if (!dataParsed.address) {
+        throw new BadRequestException('Address is required');
+      }
+
+      if (!dataParsed.version) {
+        throw new BadRequestException('Compiler version is required');
+      }
+
+      if (!dataParsed.name) {
+        throw new BadRequestException('Contract name is required');
+      }
+      if (file) {
+        const jsonContents = file.buffer.toString('utf-8');
+        const { sources, settings } = JSON.parse(jsonContents);
+        if (!sources || !settings) {
+          console.error('Invalid JSON: Missing sources or settings');
+          throw new BadRequestException(
+            'Error, missing sources or settings in file',
+          );
+        }
+      } else {
+        if (!dataParsed.source) {
+          throw new BadRequestException(
+            'Either source or sources must be provided',
+          );
+        }
+      }
+
+      const contract = await this.prisma.contract.findUnique({
+        where: {
+          address: dataParsed.address,
+        },
+      });
+
+      if (!contract) {
+        throw new BadRequestException('Contract not found in the database');
+      }
+      dataParsed.bytecode = contract.deployedCode;
+
+      const formData = new FormData();
+      formData.append('data', JSON.stringify(dataParsed));
+      if (file) {
+        const fileBlob = new Blob([file.buffer], { type: file.mimetype });
+        formData.append('file', fileBlob, file.originalname);
+      }
+
+      const response = await fetch(
+        `${process.env.VERIFIER_URL}/api/verifier/verify`,
+        {
+          method: 'POST',
+          body: formData,
+        },
+      );
+      const result = await response.json();
+      const match = await this.checkResult(result);
+
+      return {
+        success: match,
+        message: match
+          ? 'Contract verified successfully'
+          : 'Contract verification failed',
+        data: {
+          address: result.address,
+          version: result.version,
+          name: result.name,
+          dataResponse: result.data ?? 'No data',
+        },
+      };
+    } catch (error) {
+      console.error('Error in verify function:', error);
+      throw new BadRequestException('Error processing FormData');
+    }
+  }
+  private async checkResult({ bytecodeHash, resultBytecodeHash }) {
+    try {
+      if (!bytecodeHash || bytecodeHash.length !== 66)
+        throw new Error('Invalid bytecodeHash');
+      if (!resultBytecodeHash) throw new Error('resultBytecodeHash is empty');
+      return resultBytecodeHash === bytecodeHash;
+    } catch (err) {
+      throw new Error('error checking result: ' + err);
+    }
   }
 }
