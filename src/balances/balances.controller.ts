ec46d2179f14ba743ca24281b89e30dc029b5119
Sebastian Guaqueta <sebastian.guaqueta@rootstocklabs.com>
2025-02-06 14:53:24 -0500

    adding controller JSDoc to controller

--- src/balances/balances.controller.ts
+++ src/balances/balances.controller.ts
@@ -6,6 +6,13 @@ import { TAKE_PAGE_DATA } from 'src/common/constants';
 export class BalancesController {
   constructor(private balanceService: BalancesService) {}
 
+  /**
+   * Fetch a paginated list of balances using keyset pagination.
+   * @param {string} address - The address to fetch balances for.
+   * @param {number} take - Number of records to retrieve.
+   * @param {number} cursor - The block number to start from (optional).
+   * @returns Paginated balances data.
+   */
   @Get('/address/:address')
   getAllAddresses(
     @Param('address') address: string,
