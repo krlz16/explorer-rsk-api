56745db789a3a43b5f711238dcd546ae6766b866
ivegabr <isidro.vega@iovlabs.org>
2025-02-06 17:47:34 +0100

    add method description

--- src/addresses/addresses.controller.ts
+++ src/addresses/addresses.controller.ts
@@ -8,6 +8,13 @@ export class AddressesController {
 
   constructor(private addressService: AddressesService) {}
 
+  /**
+   * Fetches a paginated list of all addresses.
+   *
+   * @param {number} [take] - Number of addresses to retrieve per request. Defaults to TAKE_PAGE_DATA if not provided.
+   * @param {number} [cursor] - The ID of the last fetched address to support keyset pagination. Retrieves addresses with IDs less than this value.
+   * @returns A paginated list of addresses with associated details like balances and block numbers.
+   */
   @Get()
   getAllAddresses(
     @Query('take') take?: number,
@@ -17,11 +24,24 @@ export class AddressesController {
     return this.addressService.getAddresses(takeData, cursor);
   }
 
+  /**
+   * Fetches detailed information about a specific address.
+   *
+   * @param {string} address - The blockchain address to retrieve details for.
+   * @returns Address details, including balance, transactions, contract information (if applicable), and block number.
+   */
   @Get(':address')
   getAddress(@Param('address') address: string) {
     this.logger.log(`Fetching address details for ${address}`);
     return this.addressService.getAddress(address);
   }
+
+  /**
+   * Fetches the verification status of a specific contract address.
+   *
+   * @param {string} address - The contract address to check verification status for.
+   * @returns Verification status of the contract.
+   */
   @Get('verification/:address')
   getContractVerification(@Param('address') address: string) {
     return this.addressService.getContractVerification(address);
