39a63cd0cdc6d5374cd14a5e7ef46f713b35fcbb
ivegabr <isidro.vega@iovlabs.org>
2025-02-17 11:24:04 +0100

    add response fields

--- src/common/parsers/token-parser.service.ts
+++ src/common/parsers/token-parser.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import BigNumber from 'bignumber.js';
 
 @Injectable()
 export class TokenParserService {
@@ -9,14 +10,21 @@ export class TokenParserService {
       const { balance, blockNumber } =
         item.address_latest_balance_address_latest_balance_addressToaddress;
 
-      const symbol = item?.contract_contract_addressToaddress.symbol || null;
+      const symbol =
+        item?.contract_contract_addressToaddress.symbol || '(Not provided)';
+
+      const name = item?.name || '(Not provided)';
+
+      const balanceFormatted = balance
+        ? new BigNumber(balance, 16).dividedBy(1e18).toNumber()
+        : 0;
 
       return {
         id: item.id,
-        name: item.name,
+        name,
         address: item.address,
         symbol,
-        balance,
+        balance: balanceFormatted,
         blockNumber,
       };
     });
