e763ede0036c43fc5c3d7c45d57d19307c843fb4
krlz16 <149804324+krlz16@users.noreply.github.com>
2025-02-18 11:58:33 -0600

    Fix/dv 782 transactions defects (#18)
    * feat: add event and transaction parser services, enhance event formatting, and update event retrieval logic
    
    * fix: update default port from 5001 to 5000
    
    * fix: correct typo in transaction formatting method name
    
    * fix: correct typo in transaction formatting method name
    
    * feat: add JSDoc comments for fetching transfer events by ID
    
    * fix: update endpoint path for fetching transfer events and add contract interface selection
    
    * refactor: reorganize event parser service and update import paths also tests added
    
    ---------
    
    Co-authored-by: Carlos Rosado <carlos.rosado@rootstocklabs.com>

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,9 +3,15 @@ import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { PrismaService } from 'src/prisma.service';
 import { PaginationService } from 'src/common/pagination/pagination.service';
+import { EventParserService } from 'src/events/parser/event-parser.service';
 
 @Module({
-  providers: [EventsService, PrismaService, PaginationService],
+  providers: [
+    EventsService,
+    PrismaService,
+    PaginationService,
+    EventParserService,
+  ],
   controllers: [EventsController],
 })
 export class EventsModule {}
