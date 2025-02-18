d971932168df9d8882295b89acc850a6795dfaf3
ivegabr <isidro.vega@iovlabs.org>
2025-02-18 13:41:15 +0100

    add getAccountsByToken

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { AccountsService } from './accounts.service';
+import { PaginationTakeValidationPipe } from 'src/common/pipes/pagination-take.pipe';
+import { AddressValidationPipe } from 'src/common/pipes/address-validation.pipe';
 
 @Controller('accounts')
-export class AccountsController {}
+export class AccountsController {
+  constructor(private accountsService: AccountsService) {}
+
+  @Get(':address')
+  getAccounts(
+    @Param('address', AddressValidationPipe) address: string,
+    @Query('take', PaginationTakeValidationPipe) take?: number,
+    @Query('cursor') cursor?: string,
+  ) {
+    return this.accountsService.getAccountsByToken(address, take, cursor);
+  }
+}
